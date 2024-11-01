085888b0f6a98fcd42ce90b23e0607570e5da56d
Motasem kharouf <156711325+Motasemkhraouf@users.noreply.github.com>
2024-11-01 16:06:30 +0200

    Add files via upload

---
+++ assets/java/main.js
@@ -0,0 +1,10 @@
+let emg= document.querySelector('.ii');
+let container = document.querySelector('.container')
+function changeimage(value)
+{
+    emg.src=value;
+}
+function changeback (color)
+{
+    container.style.background = color;
+}
